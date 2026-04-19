85c868a92326380deeb71a88c351e1ba083094b1
leonovaaa1 <mariel3onova@yandex.ru>
2026-04-19 15:17:05 +0300

    Update Blue.cs

--- Lab9/Blue/Blue.cs
+++ Lab9/Blue/Blue.cs
@@ -1,3 +1,22 @@
-﻿namespace Lab9.Blue
+namespace Lab9.Blue
 {
+    public abstract class Blue
+    {
+        private string input;
+
+        public string Input => input;
+
+        protected Blue(string text)
+        {
+            input = text;
+        }
+
+        public abstract void Review();
+
+        public virtual void ChangeText(string text)
+        {
+            input = text;
+            Review(); 
+        }
+    }
 }
